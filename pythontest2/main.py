dbb0924f546fb9f72eb83de083439bf08c6e75e2
CentralnaCenzura <95255037+CentralnaCenzura@users.noreply.github.com>
2024-03-12 14:53:03 +0100

    Add files via upload

---
+++ pythontest2/main.py
@@ -0,0 +1,18 @@
+#testpy2
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def hello() -> str:
+    """Return a friendly HTTP greeting.
+    
+    Returns:
+        A string with the words 'Hello World!'.
+    """
+    return "Hello World!"
+#ssl hash key, cicd, version control 
+
+if __name__ == "__name__":
+    app.run(host="127.0.0.1", port=8800, debug=True)
